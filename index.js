47ce416aa773e97afb979f7e1dbf7e3bd6cd082f
Riley Butterfield <rileybutterfield1@gmail.com>
2020-04-23 16:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // add solution here
-function theBeatlesPlay([musicians], [instruments]){
-  var array = [];
-  
+function theBeatlesPlay(musicians, instruments){
+  let array = []
+  for (let i = 0; i < musicians.length; i++){
+    let player = musicians[i]
+    let instrument = instruments[i]
+    let string = `${player} plays ${instrument}`
+    result.push(string)
+  }
+  return array
 }
